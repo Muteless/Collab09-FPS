adc13fe7734200262bbfa949ea840e468a5cf30d
Mute <101053146+Muteless@users.noreply.github.com>
2025-03-03 14:06:32 +0000

    Initial Commit

---
+++ Source/Collab09FPS/Collab09FPS.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Collab09FPS.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Collab09FPS, "Collab09FPS" );
---
+++ Source/Collab09FPSEditor.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class Collab09FPSEditorTarget : TargetRules
+{
+	public Collab09FPSEditorTarget( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
+		ExtraModuleNames.Add("Collab09FPS");
+	}
+}
