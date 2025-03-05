0d841980ac4986e95b768f7793dc64b24b7daf1b
Mute <101053146+Muteless@users.noreply.github.com>
2025-03-05 12:00:35 +0000

    WeaponBase
    The WeaponBase class is an implementation of a generic base weapon for the game.
    It's intended to be used as a parent class from which specific weapons can derive from. It provides basic characteristics common to all weapons such as rate of fire, reload speed, and the projectile it fires.
    
    Key Features:
    ProjectileClass: A UClass pointer used to specify the type of the projectile that the weapon fires.
    RateOfFire: A float variable that represents the rate at which the weapon can fire projectiles. It is measured in x per second projectiles spawned.
    ReloadSpeed: A float variable that represents the time passed for reloading the weapon. It is measured in seconds.

---
+++ Source/Collab09FPS/Private/Weapon/WeaponBase.cpp
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Collab09FPS/Public/Weapon/WeaponBase.h"
+
+
+// Sets default values
+AWeaponBase::AWeaponBase()
+{
+	// Default variables
+	ProjectileClass = nullptr;
+	RateOfFire = BaseRateOfFire;
+	ReloadSpeed = BaseReloadSpeed;
+
+	// Bind delegates
+	
+}
+
+// Called when the game starts or when spawned
+void AWeaponBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
