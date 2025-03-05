ca9c7fb11ad731fb19205487a2b48a3c8d88b161
Mute <101053146+Muteless@users.noreply.github.com>
2025-03-05 12:04:07 +0000

    Removed Tick On Projectile Base

--- Source/Collab09FPS/Private/Projectile/ProjectileBase.cpp
+++ Source/Collab09FPS/Private/Projectile/ProjectileBase.cpp
@@ -7,8 +7,7 @@
 // Sets default values
 AProjectileBase::AProjectileBase()
 {
-	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	
 }
 
 // Called when the game starts or when spawned
@@ -17,10 +16,3 @@ void AProjectileBase::BeginPlay()
 	Super::BeginPlay();
 	
 }
-
-// Called every frame
-void AProjectileBase::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-}
-
