96c083c35162ff9b0703eeb7312f3bc8508fa00b
Mute <101053146+Muteless@users.noreply.github.com>
2025-03-05 15:07:23 +0000

    Update to WeaponBase
    In the Collab09FPS.Build.cs file, the list of module names in PublicDependencyModuleNames is updated with some spacing correction, and new module names (including "GameplayAbilities", "GameplayTags", "GameplayTasks", and "Collab09FPS") are added to PrivateDependencyModuleNames.
    
    In WeaponBase.cpp, the BeginPlay method is modified and three new methods - Fire, Melee, and Reload are added. The Fire method handles object spawning for the game, if the ProjectileClass is not null.
    The WeaponBase.h file has received some updates:
    It includes new imports for SkeletalMeshComponent.h, ArrowComponent.h, and ProjectileBase.h.
    A new delegate FOnMelee is declared.
    The ProjectileClass property is moved under the group "Weapon | Firing | Projectile".
    A new UArrowComponent property, ProjectileSpawnLocation, is created under the group "Weapon | Firing | Projectile".
    A new USkeletalMeshComponent property, WeaponMesh, is created under the group "Weapon".
    The BaseRateOfFire and RateOfFire properties are moved under the group "Weapon | Firing |".
    The BaseReloadSpeed and ReloadSpeed properties are moved under the group "Weapon | Firing |".
    New properties for ammo count - MaxAmmoAmount and CurrentAmmoAmount - are introduced under the group "Weapon | Firing |".
    The Fire, Melee, and Reload functions are declared.

--- Source/Collab09FPS/Private/Weapon/WeaponBase.cpp
+++ Source/Collab09FPS/Private/Weapon/WeaponBase.cpp
@@ -11,7 +11,16 @@ AWeaponBase::AWeaponBase()
 	ProjectileClass = nullptr;
 	RateOfFire = BaseRateOfFire;
 	ReloadSpeed = BaseReloadSpeed;
+	CurrentAmmoAmount = MaxAmmoAmount;
 
+	// Setup weapon skeletal mesh
+	WeaponMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Weapon Mesh"));
+	
+	// Setup projectile spawn location
+	ProjectileSpawnLocation = CreateDefaultSubobject<UArrowComponent>(TEXT("Projectile Spawn Location"));
+	ProjectileSpawnLocation->ArrowSize = 0.5;
+	ProjectileSpawnLocation->ArrowColor = FColor::FromHex("FFFF00FF");
+	
 	// Bind delegates
 	
 }
@@ -21,4 +30,34 @@ void AWeaponBase::BeginPlay()
 {
 	Super::BeginPlay();
 	
+}
+
+void AWeaponBase::Fire()
+{
+	// If projectile class is valid
+	if (ProjectileClass != nullptr)
+	{
+		// Setup spawn parameters
+		FActorSpawnParameters SpawnParams;
+		SpawnParams.Owner = this;
+		SpawnParams.Instigator = GetInstigator();
+
+		// Get spawn location from ProjectileSpawnLocation
+		const FVector SpawnLocation = ProjectileSpawnLocation->GetComponentLocation();
+
+		//Get spawn rotation from ProjectileSpawnLocation
+		const FRotator SpawnRotation = ProjectileSpawnLocation->GetComponentRotation();
+
+		// Spawn projectile at ProjectileSpawn
+		GetWorld()->SpawnActor<AProjectileBase>(ProjectileClass, SpawnLocation, SpawnRotation, SpawnParams);
+	}
+}
+
+void AWeaponBase::Melee()
+{
+	
+}
+
+void AWeaponBase::Reload()
+{
 }
--- Source/Collab09FPS/Public/Weapon/WeaponBase.h
+++ Source/Collab09FPS/Public/Weapon/WeaponBase.h
@@ -3,14 +3,20 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "DiffResults.h"
+
 #include "Collab09FPS/Public/Projectile/ProjectileBase.h"
 #include "GameFramework/Actor.h"
+
+#include "Components/SkeletalMeshComponent.h"
+#include "Components/ArrowComponent.h"
+#include "Projectile/ProjectileBase.h"
+
 #include "WeaponBase.generated.h"
 
 // Delegates
 DECLARE_DYNAMIC_MULTICAST_DELEGATE(FWeaponFired);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE(FWeaponReloaded);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnMelee);
 
 
 UCLASS(Abstract)
@@ -29,24 +35,64 @@ public:
 	UPROPERTY(BlueprintAssignable)
 	FWeaponReloaded WeaponReloaded;
 
+	// Called when a weapon has used melee attack (Weapon melee delegate)
+	UPROPERTY(BlueprintAssignable)
+	FWeaponReloaded OnMelee;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 	
-	// Projectile class shot by the weapon
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon | Firing")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite,
+		Category = "Weapon | Firing | Projectile")
 	TSubclassOf<class AProjectileBase> ProjectileClass;
+
+	// Location used when spawning the projectile
+	UPROPERTY(VisibleAnywhere,
+		meta=(DisplayName="Projectile Spawn Location"),
+		Category="Weapon | Firing | Projectile")
+	UArrowComponent* ProjectileSpawnLocation;
+
+	// Skeletal mesh used by the weapon
+	UPROPERTY(VisibleAnywhere,
+		meta=(DisplayName="Weapon Mesh"),
+		Category="Weapon | ")
+	class USkeletalMeshComponent* WeaponMesh;
 	
 	// Rate of fire (per Second)
-	UPROPERTY(BlueprintReadWrite, meta=(DisplayName="RateOfFire"), Category="Weapon | Firing")
+	UPROPERTY(BlueprintReadWrite,
+		meta=(DisplayName="Rate Of Fire"),
+		Category="Weapon | Firing |")
 	float BaseRateOfFire;
 	float RateOfFire;
 	
-
 	// Reloading speed in seconds
-	UPROPERTY(BlueprintReadWrite, meta=(DisplayName="ReloadSpeed"), Category="Weapon | Firing")
+	UPROPERTY(BlueprintReadWrite,
+		meta=(DisplayName="Reload Speed"),
+		Category="Weapon | Firing |")
 	float BaseReloadSpeed;
 	float ReloadSpeed;
+
+	// Max ammo count
+	UPROPERTY(BlueprintReadWrite,
+		meta=(DisplayName="Max Ammo"),
+		Category="Weapon | Firing |")
+	float MaxAmmoAmount;
+	float CurrentAmmoAmount;
+
+	
+	
+	// Function to shoot projectile
+	UFUNCTION(BlueprintCallable)
+	void Fire();
+
+	// Function to melee attack
+	UFUNCTION(BlueprintCallable)
+	static void Melee();
+
+	// Function to reload weapon
+	UFUNCTION(BlueprintCallable)
+	static void Reload();
 	
 private:
 	GENERATED_BODY()
