ab1737dfa1187677a6ad4d2fec32c5fdd0335568
Chromium <62724067+Chromum@users.noreply.github.com>
2025-03-06 22:54:24 +0000

    Added Comments

--- Source/Collab09FPS/Private/AIBase/BaseAI.cpp
+++ Source/Collab09FPS/Private/AIBase/BaseAI.cpp
@@ -11,15 +11,16 @@
 // Sets default values
 ABaseAI::ABaseAI()
 {
+	//Creation of Blackboard and Behaviour Tree
 	BehaviorTreeComponent = CreateDefaultSubobject<UBehaviorTreeComponent>(TEXT("Behavior Tree Component"));
 	BlackboardComponent = CreateDefaultSubobject<UBlackboardComponent>(TEXT("Blackboard Component"));
 }
 
-// Called when the game starts or when spawned
 void ABaseAI::BeginPlay()
 {
 	Super::BeginPlay();
 
+	//Starts running the behaviour tree
 	if (IsValid(BehaviorTree.Get()))
 	{
 		RunBehaviorTree(BehaviorTree.Get());
@@ -29,6 +30,7 @@ void ABaseAI::BeginPlay()
 
 void ABaseAI::OnPossess(APawn* InPawn)
 {
+	//Initial set up of blackboard, the blackboard WILL not work unless this is run first
 	if (IsValid(Blackboard.Get()) && IsValid(BehaviorTree.Get()))
 	{
 		Blackboard->InitializeBlackboard(*BehaviorTree.Get()->BlackboardAsset.Get());		
