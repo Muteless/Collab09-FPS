edeaa7916370197a5729b795ad3a404f4e3cda98
Mute <101053146+Muteless@users.noreply.github.com>
2025-03-12 14:41:38 +0000

    added damage execution class and health execution class

---
+++ Source/Collab09FPS/Public/GAS/EffectExecutions/MagnitudeModifierCalculations/HealthMMC.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayModMagnitudeCalculation.h"
+#include "HealthMMC.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COLLAB09FPS_API UHealthMMC : public UGameplayModMagnitudeCalculation
+{
+public:
+	UHealthMMC();
+	
+	FGameplayEffectAttributeCaptureDefinition HealDef;
+	virtual float CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const override;
+protected:
+private:
+	GENERATED_BODY()
+	
+};
