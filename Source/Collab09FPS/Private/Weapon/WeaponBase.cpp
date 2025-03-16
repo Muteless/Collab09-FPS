f88f06ccbe156cbe3358b2eac7f83a4d39a03db4
Mute <101053146+Muteless@users.noreply.github.com>
2025-03-16 16:53:05 +0000

    (WIP) added gun base

---
+++ Source/Collab09FPS/Private/Weapon/WeaponBase.cpp
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Weapon/WeaponBase.h"
+
+#include "InterchangeResult.h"
+
+
+// Sets default values
+AWeaponBase::AWeaponBase():
+	bGunMode(false)
+{
+	
+}
+
+void AWeaponBase::WeaponFire_Implementation()
+{
+	
+}
+
+void AWeaponBase::WeaponReload_Implementation()
+{
+	
+}
---
+++ Source/Collab09FPS/Public/Weapon/WeaponBase.h
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+
+#include "Interfaces/WeaponInput.h"
+
+#include "Components/SkeletalMeshComponent.h"
+#include "Weapon/GunBase.h"
+
+#include "WeaponBase.generated.h"
+
+UCLASS(Abstract)
+class COLLAB09FPS_API AWeaponBase : public AActor
+{
+public:
+	// Sets default values for this actor's properties
+	AWeaponBase();
+
+	UFUNCTION()
+	void WeaponFire_Implementation();
+
+	UFUNCTION()
+	void WeaponReload_Implementation();
+
+protected:
+	UPROPERTY(EditDefaultsOnly,
+		BlueprintReadWrite)
+	TArray<TSubclassOf<AGunBase>> GunClass;
+
+	UPROPERTY(EditAnywhere,
+		BlueprintReadWrite)
+	bool bGunMode;
+	
+private:
+	GENERATED_BODY()
+};
