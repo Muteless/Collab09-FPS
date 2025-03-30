49e4a94d74932e5479a492df74e104d50fc08515
Mute <101053146+Muteless@users.noreply.github.com>
2025-03-30 11:44:26 +0100

    (wip) added world ui objects

---
+++ Source/Collab09FPS/Private/WorldObjects/UI/StartGameWorldUI.cpp
@@ -0,0 +1,12 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WorldObjects/UI/StartGameWorldUI.h"
+
+
+// Sets default values
+AStartGameWorldUI::AStartGameWorldUI():
+WorldToTransitionTo(nullptr)
+{
+	
+}
