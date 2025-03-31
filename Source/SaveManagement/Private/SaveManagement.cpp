cd06d0b3c3c470f5886f5486f6dc52fe3b6d05ef
Mute <101053146+Muteless@users.noreply.github.com>
2025-03-31 15:58:20 +0100

    adding edit utilities

---
+++ Source/SaveManagement/Private/SaveManagement.cpp
@@ -0,0 +1,46 @@
+﻿#include "SaveManagement.h"
+
+#include "LevelEditor.h"
+
+#define LOCTEXT_NAMESPACE "FSaveManagementModule"
+
+void FSaveManagementModule::StartupModule()
+{
+	// Build the bar extension
+    FLevelEditorModule& LevelEditorModule = FModuleManager::LoadModuleChecked<FLevelEditorModule>("LevelEditor");
+	const TSharedPtr<FExtender> MenuExtender = MakeShareable(new FExtender());
+
+	MenuExtender->AddMenuBarExtension(
+		"Help",
+		EExtensionHook::After,
+		nullptr,
+		FMenuBarExtensionDelegate::CreateRaw(this, &FSaveManagementModule::AddMenu)
+		);
+	
+	LevelEditorModule.GetMenuExtensibilityManager()->AddExtender(MenuExtender);
+}
+
+void FSaveManagementModule::AddMenu(FMenuBarBuilder& MenuBarBuilder)
+{
+	// Add pull down menu
+	MenuBarBuilder.AddPullDownMenu(
+		ToolBarMenuName,
+		ToolBarMenuTooltip,
+		FNewMenuDelegate::CreateRaw(this, &FSaveManagementModule::FillMenu)
+		);
+}
+
+void FSaveManagementModule::FillMenu(FMenuBuilder& MenuBuilder)
+{
+	// Fill pull down menu
+	// MenuBuilder.AddMenuEntry(ToolBarResetSaveEntry, ToolBarResetSaveEntryTooltip)
+}
+
+void FSaveManagementModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FSaveManagementModule, SaveManagement)
---
+++ Source/SaveManagement/Public/SaveManagement.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Modules/ModuleManager.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(LogSaveManagement, All, All);
+
+class FSaveManagementModule : public IModuleInterface
+{
+public:
+    virtual void StartupModule() override;
+    virtual void ShutdownModule() override;
+
+    FText ToolBarMenuName = FText::FromString("Save Manager");
+    FText ToolBarMenuTooltip = FText::FromString("Save file editing tools");
+    
+    FText ToolBarResetSaveEntry = FText::FromString("Reset Save");
+    FText ToolBarResetSaveEntryTooltip = FText::FromString("Resets the save at the current slot");
+    
+    // Adds menu to toolbar
+    void AddMenu(FMenuBarBuilder& MenuBarBuilder);
+
+    // Populates menu in toolbar
+    void FillMenu(FMenuBuilder& MenuBuilder);
+};
