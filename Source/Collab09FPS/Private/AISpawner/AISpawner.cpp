4d00a1c5e43cfd45e98d07afd398c33f34a10463
Chromium <62724067+Chromum@users.noreply.github.com>
2025-04-30 19:35:42 +0100

    Made Spawners face enemies correctly. Added IEnable and IDisable to triggers

--- Source/Collab09FPS/Private/AISpawner/AISpawner.cpp
+++ Source/Collab09FPS/Private/AISpawner/AISpawner.cpp
@@ -111,10 +111,15 @@ void AAISpawner::SpawnEnemy()
 			FActorSpawnParameters SpawnParams;
 			SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
 
+			FVector ForwardVector = GetActorForwardVector();
+			FRotator SpawnRotation = ForwardVector.Rotation();
+
 			AActor* SpawnedEnemy = World->SpawnActor<AActor>(
-				EnemySpawnType, AdjustedSpawnLocation,
-				FRotationMatrix::MakeFromX(ArrowComponent->GetForwardVector()).Rotator(),
+				EnemySpawnType,
+				AdjustedSpawnLocation,
+				SpawnRotation,
 				SpawnParams);
+
 			
 			if (IsValid(SpawnedEnemy))
 			{
